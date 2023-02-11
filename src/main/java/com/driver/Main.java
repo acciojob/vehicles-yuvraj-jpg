028420fadcc828a5db5f695d1e1eab56ac87b5c7
Yuvraj <yv96875@gmail.com>
2023-02-11 12:36:00 +0530

    Vehicle

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,6 +1,7 @@
 package com.driver;
 
-public class Main {
+public class
+Main {
     public static void main(String[] args) {
         F1 mercedes = new F1("mercedes", false);
         mercedes.move(40, 0);
