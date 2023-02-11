a7fd3e347d3e4d414fc8d43f2eb9e8220fa2c45a
Yuvraj <yv96875@gmail.com>
2023-02-11 13:30:13 +0530

    Vehicle

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -5,6 +5,22 @@ public class Boat implements WaterVehicle{
 
     private int capacity;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
     public Boat(String name, int capacity) {
         this.name = name;
         this.capacity = capacity;
@@ -12,11 +28,11 @@ public class Boat implements WaterVehicle{
 
     @Override
     public int getVehicleCapacity() {
-        return capacity;
+        return getCapacity();
     }
 
     @Override
     public String getVehicleName() {
-        return name;
+        return getName();
     }
 }
